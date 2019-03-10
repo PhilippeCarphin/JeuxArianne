b4f387ea3c8f65b0b52f210e0b19e48ac1a34f36
Philippe Carphin <philippe.carphin@polymtl.ca>
2019-03-10 03:38:57 -0400

    WIP NUMPY

--- jeuxarianne.py
+++ jeuxarianne.py
@@ -1,31 +1,52 @@
 import itertools
 from copy import deepcopy
+import numpy as np
 
 class JABoard:
 
     def __init__(self, board_dimensions=(3,3)):
         self.board_dimensions = board_dimensions
+        self._edges = np.array(self.board_dimensions)
+        self._squares = np.zeros((self.board_dimensions[0]-1, self.board_dimensions[1]-1), dtype=np.int)
         self.edges = {}
         self.squares = {}
         self.turn = 0
         self._score = 0
+        self._nodes = np.array(self.board_dimensions)
+        self._edges = np.zeros(self.board_dimensions + self.board_dimensions, dtype=np.int)
+
+    def connect_nodes(self, n1, n2):
+        self._edges[n1[0],n1[1],n2[0], n2[1]] = 1
+
+    def disconnect_nodes(self, n1, n2):
+        self._edges[n1[0],n1[1],n2[0], n2[1]] = 0
+
+    def nodes_connected(self, n1, n2):
+        return self._edges[n1[0],n1[1],n2[0], n2[1]]
+
+    def _v_edge(self, I, J):
+        node1 = (I,J)
+        node2 = (I+1,J)
+        return node1, node2
+
+    def _h_edge(self, I, J):
+        node1 = (I,J)
+        node2 = (I,J+1)
+        return node1, node2
+
+    def _square_is_surrounded(self, I, J):
+        return (self._edges[I,J,I+1,J]
+                and self._edges[I,J,I,J+1]
+                and self._edges[I+1,J,I+1,J+1]
+                and self._edges[I,J+1,I+1,J+1])
 
     @property
     def score(self):
-        score = 0
-        for s in self.squares:
-            square_owner = self.squares[s]
-            if square_owner == 0:
-                score += 1
-            elif square_owner == 1:
-                score -= 1
-        return score
-
+        return np.sum(self._squares)
 
     def in_board(self, point):
         return (0 <= point[0] < self.board_dimensions[0]
                 and 0 <= point[1] < self.board_dimensions[1])
-
 
     def edge_is_legal(self, edge):
         if not isinstance(edge, (tuple,tuple)):
@@ -59,21 +80,23 @@ class JABoard:
     def get_max(self, depth):
         moves = []
         for m in self.possible_moves():
-            board = self.try_edge(m, 0)
+            new_marked = self.play_edge(m,1)
             print("{}trying edge {}".format(" " * depth, m))
-            moves.append((m, board.get_value(depth), 0))
+            moves.append((m, self.get_value(depth), 1))
+            self.unplay_edge(m,1,new_marked)
 
         if not moves:
-            return (None, self.score, 0)
+            return (None, self.score, 1)
 
         return max(moves, key=lambda m: m[1])
 
     def get_min(self, depth):
         moves = []
         for m in self.possible_moves():
-            board = self.try_edge(m, 1)
             print("{}trying edge {}".format(" " * depth, m))
-            moves.append((m, board.get_value(depth), 1))
+            new_marked = self.play_edge(m,-1)
+            moves.append((m, self.get_value(depth), -1))
+            self.unplay_edge(m,-1, new_marked)
         if not moves:
             return (None, self.score, 1)
         return min(moves, key=lambda m: m[1])
@@ -104,20 +127,26 @@ class JABoard:
                 and edge4 in self.edges)
 
     def mark_completed_squares(self, player, last_edge_played):
+        assert(player == -1 or player == 1)
         di = last_edge_played[0][0] - last_edge_played[1][0]
         dj = last_edge_played[0][1] - last_edge_played[1][1]
+        new_marked = []
         if di == 0:
             minj = min(last_edge_played[0][1], last_edge_played[1][1])
             i = last_edge_played[0][0]
             upper_square = (i - 1, minj)
             lower_square = (i, minj)
-            if i < self.board_dimensions[0] - 1 and self.square_is_surrounded(lower_square):
-                if not lower_square in self.squares:
-                    self.squares[lower_square] = player
+            if i < self.board_dimensions[0] - 1 and self._square_is_surrounded(i, minj):
+                # TODO Make a numpy array for this
+                if not self._squares[i,minj]:
+                    self._squares[i,minj] = player
+                    new_marked.append((i,minj))
 
-            if upper_square[0] >= 0 and self.square_is_surrounded(upper_square):
-                if not upper_square in self.squares:
-                    self.squares[upper_square] = player
+            if upper_square[0] >= 0 and self._square_is_surrounded(i-1, minj):
+                if not self._squares[i-1,minj]:
+                    self._squares[i-1,minj] = player
+                    new_marked.append((i-1,minj))
+
         if dj == 0:
             mini = min(last_edge_played[0][0], last_edge_played[1][0])
             j = last_edge_played[0][1]
@@ -125,12 +154,16 @@ class JABoard:
             right_square = (mini, j)
             if (j < self.board_dimensions[1] -1
                     and self.square_is_surrounded(right_square)):
-                if not right_square in self.squares:
-                    self.squares[right_square] = player
+                if not self._squares[mini, j]:
+                    new_marked.append((mini,j))
+                    self._squares[mini, j] = player
 
             if left_square[1] >= 0 and self.square_is_surrounded(left_square):
-                if not left_square in self.squares:
-                    self.squares[left_square] = player
+                if not self._squares[mini, j-1]:
+                    new_marked.append((mini,j-1))
+                    self._squares[mini, j-1] = player
+
+        return new_marked
 
 
 
@@ -138,8 +171,18 @@ class JABoard:
         assert(edge not in self.edges)
         assert(self.edge_is_legal(edge))
         self.edges[edge] = True
-        self.mark_completed_squares(player, edge)
+        self.connect_nodes(edge[0], edge[1])
+        new_marked = self.mark_completed_squares(player, edge)
+
         self.turn = 0 if self.turn == 1 else 1
+        return new_marked
+
+    def unplay_edge(self, edge, player, new_marked):
+        self.disconnect_nodes(edge[0], edge[1])
+        for square in new_marked:
+            self._squares[square[0], square[1]] = 0
+        self.turn = 0 if self.turn == 1 else 1
+
 
     def try_edge(self, edge, player):
         board = deepcopy(self)
@@ -159,8 +202,8 @@ class JABoard:
             for j in range(self.board_dimensions[1]):
                 dl += "*"
                 if j < self.board_dimensions[1] - 1:
-                    edge = self.h_edge(i,j)
-                    if edge in self.edges:
+                    n1, n2 = self._h_edge(i,j)
+                    if self.nodes_connected(n1,n2):
                         dl += " - "
                     else:
                         dl += "   "
@@ -169,19 +212,19 @@ class JABoard:
         def squares_and_edges_line(I):
             sel = ""
             for j in range(self.board_dimensions[1]):
-                edge = self.v_edge(I,j)
+                n1, n2 = self._v_edge(I,j)
 
-                if edge in self.edges:
+                if self.nodes_connected(n1, n2):
                     sel += "|"
                 else:
                     sel += " "
 
                 if j < self.board_dimensions[1] - 1:
-                    square = (I,j)
-                    if square in self.squares:
-                        sel += " {} ".format(self.squares[square])
+                    owner = self._squares[I,j]
+                    if owner == -1:
+                        sel += " {}".format(owner)
                     else:
-                        sel += "   "
+                        sel += " {} ".format(owner)
             return sel
 
 
--- test_jeuxarianne.py
+++ test_jeuxarianne.py
@@ -20,27 +20,27 @@ class TestJABoard(unittest.TestCase):
         self.assertRaises(Exception, self.test_board.square_is_surrounded, (2,2))
 
     def test_play_edge(self):
-        self.test_board.play_edge(((0,0),(0,1)), 0)
-        self.assertRaises(AssertionError, self.test_board.play_edge, ((0,0),(0,1)), 0)
+        self.test_board.play_edge(((0,0),(0,1)), -1)
+        self.assertRaises(AssertionError, self.test_board.play_edge, ((0,0),(0,1)), -1)
 
     def play_game(self, board=None):
         if not board:
             board = self.test_board
-        board.play_edge(((0,0),(0,1)), 0)
+        board.play_edge(((0,0),(0,1)), -1)
 
         board.play_edge(((0,0),(1,0)), 1)
 
-        board.play_edge(((0,1),(1,1)), 0)
+        board.play_edge(((0,1),(1,1)), -1)
 
         board.play_edge(((1,0),(1,1)), 1)
         board.play_edge(((1,1),(1,2)), 1)
 
-        board.play_edge(((1,1),(2,1)), 0)
+        board.play_edge(((1,1),(2,1)), -1)
 
         board.play_edge(((0,1),(0,2)), 1)
 
-        board.play_edge(((0,2),(1,2)), 0)
-        board.play_edge(((1,2),(2,2)), 0)
+        board.play_edge(((0,2),(1,2)), -1)
+        board.play_edge(((1,2),(2,2)), -1)
 
         board.play_edge(((2,1),(2,2)), 1)
         board.play_edge(((2,0),(2,1)), 1)
@@ -50,7 +50,7 @@ class TestJABoard(unittest.TestCase):
     def test_str(self):
         self.play_game()
 
-        print(str(self.test_board))
+        # print(str(self.test_board))
 
     def test_different_size(self):
         self.play_game(self.bigger_board)
@@ -59,50 +59,32 @@ class TestJABoard(unittest.TestCase):
 
     def test_get_value(self):
         self.play_game(self.test_board)
-        print("get_value(): ", self.test_board.get_value())
+        # print("get_value(): ", self.test_board.get_value())
 
     def test_get_value_empty_board(self):
-        self.test_board.play_edge(((0,0),(0,1)), 0)
+        self.test_board.play_edge(((0,0),(0,1)), -1)
 
         self.test_board.play_edge(((0,0),(1,0)), 1)
 
-        self.test_board.play_edge(((0,1),(1,1)), 0)
-        self.test_board.play_edge(((1,1),(2,1)), 0)
+        self.test_board.play_edge(((0,1),(1,1)), -1)
+
+        self.test_board.play_edge(((1,0),(1,1)), -1)
+        self.test_board.play_edge(((1,1),(1,2)), 1)
+
+        self.test_board.play_edge(((1,1),(2,1)), -1)
 
         self.test_board.play_edge(((0,1),(0,2)), 1)
 
-        self.test_board.play_edge(((0,2),(1,2)), 0)
-        self.test_board.play_edge(((1,2),(2,2)), 0)
+        self.test_board.play_edge(((1,2),(2,2)), 1)
 
         self.test_board.play_edge(((2,1),(2,2)), 1)
         self.test_board.play_edge(((2,0),(2,1)), 1)
+        self.test_board.play_edge(((1,0),(2,0)), 1)
+        self.test_board.play_edge(((0,2),(1,2)), 1)
+        value = self.test_board.get_value()
+        print(self.test_board)
+        print("get_value(): ", value)
 
-        # board.play_edge(((1,0),(2,0)), 0)
-
-    def test_str(self):
-        self.play_game()
-
-        print(str(self.test_board))
-
-    def test_different_size(self):
-        self.play_game(self.bigger_board)
-        # self.assertEqual(self.bigger_board.score, 0)
-        pass
-
-    def test_get_value(self):
-        self.play_game(self.test_board)
-        print("get_value(): ", self.test_board.get_value())
-
-    def test_get_value_empty_board(self):
-        self.test_board.play_edge(((0,0),(0,1)), 0)
-
-        self.test_board.play_edge(((0,0),(1,0)), 1)
-
-        self.test_board.play_edge(((0,1),(1,1)), 0)
-        print(self.test_board.get_value())
-
-
-        print(self.test_board.get_value())
 
 
 
